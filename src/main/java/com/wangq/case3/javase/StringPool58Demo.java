038ef9eb549c020813cc4c94446e7985ba514b78
wangq-oukei <814848110@qq.com>
2021-01-16 16:57:14 +0800

    Initial commit

---
+++ src/main/java/com/wangq/case3/javase/StringPool58Demo.java
@@ -0,0 +1,21 @@
+package com.wangq.case3.javase;
+
+/**
+ * @author songdan
+ * @Created 2021-01-10 22:16
+ */
+public class StringPool58Demo {
+    public static void main(String[] args) {
+        String str1 = new StringBuilder("58").append("tongcheng").toString();
+        System.out.println(str1);
+        System.out.println(str1.intern());
+        System.out.println(str1 == str1.intern());
+
+        System.out.println("=====================");
+
+        String str2 = new StringBuilder("ja").append("va").toString();
+        System.out.println(str2);
+        System.out.println(str2.intern());
+        System.out.println(str2 == str2.intern());
+    }
+}
